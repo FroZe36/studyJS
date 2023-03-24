ab0919c84ba4e1d6c95ca6b6eac9834e6bc35659
FroZe36 <AngelofDest1ny@yandex.ru>
2023-03-24 15:29:31 +0300

    feat: add solution for lesson7_2[Udemy]

--- udemyJS/lesson7/index.js
+++ udemyJS/lesson7/index.js
@@ -52,3 +52,48 @@ function showProgrammingLangs(plan) {
 console.log(showProgrammingLangs(personalPlanPeter));
 
 console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+  return arr.length != 0 ? `Семья состоит из: ${arr.join(' ')}` : 'Семья пуста';
+}
+console.log(showFamily(family));
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+  let result = '';
+  for (let key of arr) {
+    result += `${key.toLowerCase()}\n`;
+  }
+  return result;
+}
+console.log(standardizeStrings(favoriteCities));
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+  return typeof str === 'string'
+    ? str.split('').reverse().join('')
+    : console.log('Ошибка!');
+}
+
+reverse(someString);
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+  let result = '';
+  if (arr.length !== 0) {
+    arr.forEach(element => {
+      if (element != missingCurr) {
+        result += `${element}\n`;
+      }
+    });
+  } else return 'Нет доступных валют';
+  return 'Доступные валюты:\n' + result;
+}
+
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'RUB'));
